919b87b0c12bcd13559b729bf6edbdd23c4ff9ab
Artur <art05xxx@rambler.ru>
2020-10-04 11:31:41 +0300

    Moved folder app_recruitment eighth part project

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app_recruitment.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app_recruitment_new.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
